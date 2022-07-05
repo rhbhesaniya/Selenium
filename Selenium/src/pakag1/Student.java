318e8144766e8ab59f180c7c88f8c8a984644c26
Rakesh <Rakesh@DESKTOP-KA8TNUK>
2022-07-05 20:06:00 +0530

    pushing first code

---
+++ Selenium/src/pakag1/Student.java
@@ -0,0 +1,22 @@
+package pakag1;
+
+public class Student {
+	
+	public void display()
+	{
+		System.out.println("hi");
+	}
+
+	public void display1()
+	{
+		
+		System.out.println("test");
+	}
+	
+	public static void main(String[] args) {
+		
+		Student s=new Student();
+		s.display();
+		s.display1();
+	}
+}
